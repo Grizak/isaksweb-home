43df1a65a4e076b0a3c947fb2677fb752410eb58
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2025-09-11 12:24:43 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import axios from "axios";
+import crypto from "crypto";
 
 interface Project {
   id: number;
@@ -17,6 +19,138 @@ interface Skill {
   category: "frontend" | "backend" | "tools" | "database";
 }
 
+interface GitHubLicense {
+  key: string;
+  name: string;
+  spdx_id: string;
+  url: string;
+  node_id: string;
+}
+
+interface GitHubOwner {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: "User" | "Organization";
+  user_view_type: string;
+  site_admin: boolean;
+}
+
+interface GitHubRepository {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  owner: GitHubOwner;
+  html_url: string;
+  description: string | null;
+  fork: boolean;
+  url: string;
+  forks_url: string;
+  keys_url: string;
+  collaborators_url: string;
+  teams_url: string;
+  hooks_url: string;
+  issue_events_url: string;
+  events_url: string;
+  assignees_url: string;
+  branches_url: string;
+  tags_url: string;
+  blobs_url: string;
+  git_tags_url: string;
+  git_refs_url: string;
+  trees_url: string;
+  statuses_url: string;
+  languages_url: string;
+  stargazers_url: string;
+  contributors_url: string;
+  subscribers_url: string;
+  subscription_url: string;
+  commits_url: string;
+  git_commits_url: string;
+  comments_url: string;
+  issue_comment_url: string;
+  contents_url: string;
+  compare_url: string;
+  merges_url: string;
+  archive_url: string;
+  downloads_url: string;
+  issues_url: string;
+  pulls_url: string;
+  milestones_url: string;
+  notifications_url: string;
+  labels_url: string;
+  releases_url: string;
+  deployments_url: string;
+  created_at: string; // ISO 8601 date string
+  updated_at: string; // ISO 8601 date string
+  pushed_at: string; // ISO 8601 date string
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  svn_url: string;
+  homepage: string | null;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string | null;
+  has_issues: boolean;
+  has_projects: boolean;
+  has_downloads: boolean;
+  has_wiki: boolean;
+  has_pages: boolean;
+  has_discussions: boolean;
+  forks_count: number;
+  mirror_url: string | null;
+  archived: boolean;
+  disabled: boolean;
+  open_issues_count: number;
+  license: GitHubLicense | null;
+  allow_forking: boolean;
+  is_template: boolean;
+  web_commit_signoff_required: boolean;
+  topics: string[];
+  visibility: "public" | "private" | "internal";
+  forks: number;
+  open_issues: number;
+  watchers: number;
+  default_branch: string;
+}
+
+type GithubRepositoriesResponse = GithubRepository[];
+
+const projectsFromGithub = [];
+
+const res = await axios.get<GithubRepositoriesResponse>("https://api.github.com/users/Grizak/repos");
+
+if (res.data) {
+  res.data.forEach((repo) => {
+    const newProject: Project = {
+      id: crypto.randomUUID(),
+      title: repo.name,
+      description: repo.description,
+      tech: [repo.language],
+      sourceUrl: repo.html_url,
+      featured: false,
+    }
+    projectsFromGithub.push(newProject);
+  })
+};
+
 const app = express();
 const PORT = Number(process.env.PORT || 3000);
 
@@ -29,7 +163,7 @@ app.get("/api/data", (req, res) => {
     skills: Skill[];
   } = {
     currentlyLearning: ["Typescript", "React", "TailwindCSS"],
-    projects: [],
+    projects: [...projectsFromGithub],
     skills: [],
   };
   res.json(JsonResponse);
